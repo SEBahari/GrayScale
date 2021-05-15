52ba8bbe83ebb243f0a22a82226bddf4a669bcb7
ehsan <ebaharinasab.eb@gmail.com>
2021-05-16 00:24:43 +0430

    some bugs fixed

--- js/main.js
+++ js/main.js
@@ -1,17 +1,25 @@
 const navbar = document.getElementById('navbar');
 
 if (window.innerWidth < 992) {
-    navbar.classList.remove('navbar-dark');
     navbar.classList.add('navbar-light');
+} else {
+    let st = window.pageYOffset || document.documentElement.scrollTop;
+    if (st === 0) {
+        inTop();
+    } else {
+        notInTop();
+    }
+
 }
 
 window.onresize = () => {
     if (window.innerWidth < 992) {
-        navbar.style.height = '4rem';
+        navbar.style.minHeight = '4rem';
         navbar.style.backgroundColor = '#fff';
         navbar.classList.remove('navbar-dark');
         navbar.classList.add('navbar-light');
     } else if (window.pageYOffset <= 40) {
+        navbar.style.minHeight = null;
         navbar.style.backgroundColor = 'transparent';
         navbar.classList.remove('navbar-light');
         navbar.classList.add('navbar-dark');
@@ -22,15 +30,25 @@ document.onscroll = function () {
     if (window.innerWidth > 992) {
         let st = window.pageYOffset || document.documentElement.scrollTop;
         if (st > 0) {
-            navbar.style.height = '3rem';
-            navbar.style.backgroundColor = '#fff';
-            navbar.classList.remove('navbar-dark');
-            navbar.classList.add('navbar-light');
+            notInTop();
         } else {
-            navbar.style.height = '5rem';
-            navbar.style.backgroundColor = 'transparent';
-            navbar.classList.remove('navbar-light');
-            navbar.classList.add('navbar-dark');
+            inTop();
         }
     }
 };
+
+function inTop() {
+    console.log('top');
+    navbar.style.height = '5rem';
+    navbar.style.backgroundColor = 'transparent';
+    navbar.classList.remove('navbar-light');
+    navbar.classList.add('navbar-dark');
+}
+
+function notInTop() {
+    console.log('not in top');
+    navbar.style.height = '3rem';
+    navbar.style.backgroundColor = '#fff';
+    navbar.classList.remove('navbar-dark');
+    navbar.classList.add('navbar-light');
+}
